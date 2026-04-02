9fb9624687280f3912f85273864b998f3f20e91a
Daniel Kagemann <info@danielkagemann.name>
2026-04-02 14:36:56 +0200

    chore: fixed basepath

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/comestibles",
+  assetPrefix: "/comestibles/",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
